1f11effe7d4c76c9fe4ac1d6359f4928c98e3da9
pandaman <aeonzamaek@gmail.com>
2015-09-07 23:41:19 -0400

    Predict test cluster indexes

--- main.py
+++ main.py
@@ -10,51 +10,58 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
+def plot_cluster(X_train, km):
+	fig = plt.figure(1, figsize=(4,3))
+	plt.clf()
+	ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+	plt.cla()
+	labels=km.labels_
+	ax.scatter(X_train[:, 0], X_train[:, 1], X_train[:, 2], c=labels.astype(np.float))
+	ax.w_xaxis.set_ticklabels([])
+	ax.w_yaxis.set_ticklabels([])
+	ax.w_zaxis.set_ticklabels([])
+	ax.set_xlabel('Genre')
+	ax.set_ylabel('Price')
+	ax.set_zlabel('Large area name')
+	plt.show()
 
 # Load training coupons
 # Extract features
-filename = 'data/csv/coupon_list_train.csv'
-df = pd.read_csv(filename, header=0)
-# limited_df = df[['COUPON_ID_hash', 'GENRE_NAME', 'PRICE_RATE']]
-limited_df = df[['GENRE_NAME', 'PRICE_RATE', 'large_area_name']]
-# np_array = df.as_matrix()
-# headers = list(df.columns.values)
+filename_train = 'data/csv/coupon_list_train.csv'
+df_train = pd.read_csv(filename_train, header=0)
+# limited_df_train = df_train[['COUPON_ID_hash', 'GENRE_NAME', 'PRICE_RATE']]
+limited_df_train = df_train[['GENRE_NAME', 'PRICE_RATE', 'large_area_name']]
+# np_array = df_train.as_matrix()
+# headers = list(df_train.columns.values)
 # Encode categories into integers
 genre_encoder = LabelEncoder()
 large_area_name_encoder = LabelEncoder()
 coupon_id_encoder = LabelEncoder()
 # Fit label encoder and return encoded labels
-# labels = genre_encoder.fit_transform(limited_df['GENRE_NAME'])
+# labels = genre_encoder.fit_transform(limited_df_train['GENRE_NAME'])
 # classes = label_encoder.classes_
 # print label_encoder.transform(['グルメ','グルメ'])
-# label_encoder.fit(limited_df)
+# label_encoder.fit(limited_df_train)
 # # Similarly, we can encode the original mutable data frame
-limited_df.loc[:,('GENRE_NAME')] = genre_encoder.fit_transform(limited_df['GENRE_NAME'])
-limited_df.loc[:,('large_area_name')] = large_area_name_encoder.fit_transform(limited_df['large_area_name'])
-# limited_df.loc[:,('COUPON_ID_hash')] = coupon_id_encoder.fit_transform(limited_df['COUPON_ID_hash'])
+limited_df_train.loc[:,('GENRE_NAME')] = genre_encoder.fit_transform(limited_df_train['GENRE_NAME'])
+limited_df_train.loc[:,('large_area_name')] = large_area_name_encoder.fit_transform(limited_df_train['large_area_name'])
+# limited_df_train.loc[:,('COUPON_ID_hash')] = coupon_id_encoder.fit_transform(limited_df_train['COUPON_ID_hash'])
 # Use One Hot Encoder to convert categorical features to binary features
-# matrice = OneHotEncoder().fit_transform(limited_df.as_matrix)
+# matrice = OneHotEncoder().fit_transform(limited_df_train.as_matrix)
 ## Clusterize using kmeans
-X = limited_df.as_matrix()
+X_train = limited_df_train.as_matrix()
 km = KMeans(n_clusters=8, init='k-means++', max_iter=100, n_init=1)
-km.fit(X)
+km.fit(X_train)
 # Finally, observe the result
 # print km.labels_
-fig = plt.figure(1, figsize=(4,3))
-plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-plt.cla()
-labels=km.labels_
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=labels.astype(np.float))
-plt.show()
+# plot_cluster(X_train, km)
+
 # Load testing coupons
-# Classify within cluster
-# -----------------------
-# Play
-# -----------------------
-# # Manual instanstiation of a DataFrame from a dictionary
-# features = {
-# 	'pet': ['cat', 'dog', 'dog', 'fish', 'cat', 'dog', 'cat', 'fish'],
-# 	'children': [4., 6, 3, 3, 2, 3, 5, 4],
-# 	'salary':   [90, 24, 44, 27, 32, 59, 36, 27]}
-# data = pd.DataFrame(features)
+# Classify within trainin clusters
+filename_test = 'data/csv/coupon_list_test.csv'
+df_test = pd.read_csv(filename_test, header=0)
+limited_df_test = df_test[['GENRE_NAME', 'PRICE_RATE', 'large_area_name']]
+limited_df_test.loc[:,('GENRE_NAME')] = genre_encoder.fit_transform(limited_df_test['GENRE_NAME'])
+limited_df_test.loc[:,('large_area_name')] = large_area_name_encoder.fit_transform(limited_df_test['large_area_name'])
+X_test = limited_df_test.as_matrix()
+predictions_test = km.predict(X_test)
